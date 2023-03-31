29e534c613ab95ecdf4506321702830e5e162c17
Jude Sajith <judesajith.aj@gmail.com>
2023-03-31 05:08:03 -0400

    ssss

--- pages/DocuBOT.py
+++ pages/DocuBOT.py
@@ -13,9 +13,9 @@ DATA_DIR = "data"
 
 index_filenames = [f for f in os.listdir(DATA_DIR) if f.endswith(".json")]
 selected_index = st.selectbox("Select an index to load", index_filenames)
-    
+button = st.button("Load Index")    
 
-if selected_index:
+if button:
     index_path = os.path.join(DATA_DIR, selected_index)
     index = GPTSimpleVectorIndex.load_from_disk(index_path)
     st.success("Index loaded succesfully.")
