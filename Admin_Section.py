c55a14b91525c52c3d3fa8feb9c16a85703c7e4a
Jude Sajith <judesajith.aj@gmail.com>
2023-03-31 06:57:25 -0400

    ssss

--- Admin_Section.py
+++ Admin_Section.py
@@ -2,10 +2,15 @@ import os
 import streamlit as st
 from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
 import openai
+from pathlib import Path
+from llama_index import download_loader
+
 
 
 openai.api_key = os.getenv("API_KEY")
+PDFReader = download_loader("PDFReader")
 
+loader = PDFReader()
 
 # Define the data directory path
 DATA_DIR = "data"
@@ -14,6 +19,19 @@ DATA_DIR = "data"
 if not os.path.exists(DATA_DIR):
     os.mkdir(DATA_DIR)
 
+
+def display_pdf(DATA_DIR, pdf_file):
+    with open(os.path.join(DATA_DIR, pdf_file), "rb") as f:
+        pdf_reader = PyPDF2.PdfReader (f)
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num]
+            with st.expander('Page'):
+                st.write(page.extract_text())
+
+def delete_pdf(DATA_DIR, pdf_file):
+    os.remove(os.path.join(DATA_DIR, pdf_file))
+    
+
 # Define a function to save the uploaded file to the data directory
 def save_uploaded_file(uploaded_file):
     with open(os.path.join(DATA_DIR, uploaded_file.name), "wb") as f:
@@ -21,7 +39,7 @@ def save_uploaded_file(uploaded_file):
 
 # Define the Streamlit app
 def main():
-    st.title("PDF Indexing App")
+    st.title("DocuBOT Admin")
     
     # Create a file uploader widget
     uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
@@ -46,6 +64,32 @@ def main():
         # Save the index to the data directory with the same name as the PDF
         index.save_to_disk(os.path.join(DATA_DIR, os.path.splitext(pdf_filename)[0] + ".json"))
         st.success("Index created successfully!")
+    # Get a list of files in the directory
+        files = os.listdir(DATA_DIR)
+
+        colms = st.columns((4, 1, 1))
+
+        fields = ["Name", 'View', 'Delete' ]
+        for col, field_name in zip(colms, fields):
+            # header
+            col.subheader(field_name)
+
+        i=1
+        for  Name in files:
+            i+=1
+            col1, col2, col3 = st.columns((4 , 1, 1))
+            # col1.write(x)  # index
+            col1.caption(Name)  # email
+            col2.button("View", key=Name, on_click=display_pdf, args=(DATA_DIR, Name))  # unique ID
+            # col4.button(user_table['Delete'][x])   # email status
+            delete_status = fields[0]  # flexible type of button
+            button_type = "Delete" if delete_status else "Gone"
+            button_phold = col3.empty()  # create a placeholder
+            do_action = button_phold.button(button_type, key=i, on_click=delete_pdf, args=(DATA_DIR, Name))
+            # if do_action:
+            #         pass # do some action with a row's data
+            #         button_phold.empty()  #  remove button
     
 if __name__ == "__main__":
     main()
+    
