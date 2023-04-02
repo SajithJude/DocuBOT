81c2114296a597ac6b8823b10dff47b57a272be9
Jude Sajith <judesajith.aj@gmail.com>
2023-04-02 12:56:17 -0400

    ssss

--- pages/Learner_Quiz.py
+++ pages/Learner_Quiz.py
@@ -12,7 +12,7 @@ st.header("Streamlit Chat - Demo")
 uploaded_file = st.file_uploader("Choose a file")
 
 if uploaded_file is not None:
-    questions = [q['question'] for q in json.load(uploaded_file)]
+    questions =  [q['question'] for q in json.load(uploaded_file)]
     
     if 'generated' not in st.session_state:
         st.session_state['generated'] = []
@@ -29,13 +29,14 @@ if uploaded_file is not None:
 
     if st.session_state['current_question'] < len(questions):
         current_question = questions[st.session_state['current_question']]
-        message(current_question, is_user=False, key=str(st.session_state['current_question']))
+        message(current_question['question'], is_user=False, key=str(st.session_state['current_question']))
         user_input = get_text()
 
         if user_input:
-            st.sidebar.write(current_question)
+            st.sidebar.write(current_question['question'])
             st.sidebar.write("You: ", user_input)
             
+            message(current_question['answer'], is_user=False, key=str(st.session_state['current_question']) + '_answer')
             st.session_state['current_question'] += 1
 
     if st.session_state['generated']:
