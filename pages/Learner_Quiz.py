333b5b21321810964943a61f348f7a47f0dbb38a
Jude Sajith <judesajith.aj@gmail.com>
2023-04-02 12:22:18 -0400

    ssss

--- pages/Learner_Quiz.py
+++ pages/Learner_Quiz.py
@@ -12,8 +12,7 @@ st.header("Streamlit Chat - Demo")
 uploaded_file = st.file_uploader("Choose a file")
 
 if uploaded_file is not None:
-    qna_pairs = json.load(uploaded_file)
-    questions = [qna['question'] for qna in qna_pairs]
+    questions = json.load(uploaded_file)
     
     if 'generated' not in st.session_state:
         st.session_state['generated'] = []
@@ -21,33 +20,30 @@ if uploaded_file is not None:
     if 'past' not in st.session_state:
         st.session_state['past'] = []
         
-    if 'user_answers' not in st.session_state:
-        st.session_state['user_answers'] = ['' for _ in range(len(questions))]
+    if 'current_question' not in st.session_state:
+        st.session_state['current_question'] = 0
 
     def get_text():
         input_text = st.text_input("You: ","", key="input")
         return input_text 
 
     if st.session_state['current_question'] < len(questions):
-        current_question = questions[st.session_state['current_question']]
+        current_question = questions[st.session_state['current_question']]['question']
         message(current_question, is_user=False, key=str(st.session_state['current_question']))
         user_input = get_text()
 
         if user_input:
-            st.session_state['user_answers'][st.session_state['current_question']] = user_input
             st.sidebar.write(current_question)
             st.sidebar.write("You: ", user_input)
+            st.session_state['past'].append(current_question)
+            st.session_state['past'].append(user_input)
             
             st.session_state['current_question'] += 1
 
     if st.session_state['generated']:
         for i in range(len(st.session_state['generated'])-1, -1, -1):
             message(st.session_state["generated"][i], key=str(i))
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state['past'][i*2], is_user=True, key=str(i) + '_user')
+            st.sidebar.write(questions[i]['question'])
+            st.sidebar.write("You: ", st.session_state['past'][i*2+1])
             st.sidebar.write("Bot: ", st.session_state["generated"][i])
-    
-    st.sidebar.write("Your answers:")
-    for i, qna in enumerate(qna_pairs):
-        st.sidebar.write(qna['question'])
-        st.sidebar.write("You: ", st.session_state['user_answers'][i])
-        st.sidebar.write("Bot: ", qna['answer'])
