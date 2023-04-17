cd0f290ea5187f40e66734239becac4d0a4019c0
Jude Sajith <judesajith.aj@gmail.com>
2023-04-17 11:15:19 +0530

    ssss

--- pages/DocuBOT_Quiz_Mode.py
+++ pages/DocuBOT_Quiz_Mode.py
@@ -2,6 +2,42 @@ import streamlit as streamlit
 from streamlit_chat import message
 import json
 
+DB_FILE = "db.json"
+
+
+class User:
+    def __init__(self, username, password, user_type, instructor=None, assignments=None):
+        self.username = username
+        self.password = password
+        self.user_type = user_type
+        self.instructor = instructor
+        self.assignments = assignments if assignments else []
+
+    def to_dict(self):
+        return {
+            "username": self.username,
+            "password": self.password,
+            "user_type": self.user_type,
+            "instructor": self.instructor,
+            "assignments": self.assignments,
+        }
+
+
+def load_users() -> List[User]:
+    if Path(DB_FILE).is_file():
+        with open(DB_FILE, "r") as f:
+            users_data = json.load(f)
+        return [User(**user_data) for user_data in users_data]
+    else:
+        return []
+
+
+def save_users(users: List[User]):
+    users_data = [user.to_dict() for user in users]
+    with open(DB_FILE, "w") as f:
+        json.dump(users_data, f)
+
+
 streamlit.set_page_config(
     page_title="DocuBOT QuizMode",
     page_icon=":robot:"
@@ -9,61 +45,63 @@ streamlit.set_page_config(
 
 streamlit.header("DocuBOT QuizMode")
 
-uploaded_file = streamlit.file_uploader("Upload the Json Config file here")
+users = load_users()
 
-if uploaded_file is not None:
-    questions = [q['question'] for q in json.load(uploaded_file)]
-    
-    if 'generated' not in streamlit.session_state:
-        streamlit.session_state['generated'] = []
-
-    if 'past' not in streamlit.session_state:
-        streamlit.session_state['past'] = []
+if "username" in streamlit.session_state:
+    user = [u for u in users if u.username == streamlit.session_state['username']][0]
+    if user.user_type == "student":
+        questions = user.assignments
         
-    if 'current_question' not in streamlit.session_state:
-        streamlit.session_state['current_question'] = 0
+        if 'generated' not in streamlit.session_state:
+            streamlit.session_state['generated'] = []
 
-    def get_text():
-        input_text = streamlit.text_input("You: ","", key="input")
-        return input_text 
+        if 'past' not in streamlit.session_state:
+            streamlit.session_state['past'] = []
+            
+        if 'current_question' not in streamlit.session_state:
+            streamlit.session_state['current_question'] = 0
 
-    if streamlit.session_state['current_question'] < len(questions):
-        current_question = questions[streamlit.session_state['current_question']]
-        message(current_question, is_user=False, key=str(streamlit.session_state['current_question']))
-        user_input = get_text()
+        def get_text():
+            input_text = streamlit.text_input("You: ","", key="input")
+            return input_text 
 
-        if user_input:
-            streamlit.session_state['past'].append(user_input)
-            streamlit.session_state['current_question'] += 1
+        if streamlit.session_state['current_question'] < len(questions):
+            current_question = questions[streamlit.session_state['current_question']]
+            message(current_question['question'], is_user=False, key=str(streamlit.session_state['current_question']))
+            user_input = get_text()
 
-        streamlit.sidebar.header("Conversation History")
-        for i, question in enumerate(questions):
-            if i < streamlit.session_state['current_question']:
-                streamlit.sidebar.write(question)
-                streamlit.sidebar.write("You: " + streamlit.session_state['past'][i])
+            if user_input:
+                streamlit.session_state['past'].append(user_input)
+                streamlit.session_state['current_question'] += 1
 
-    else:
-        responses = []
-        for i, question in enumerate(questions):
-            response = {
-                "question": question,
-                "response": streamlit.session_state['past'][i]
-            }
-            responses.append(response)
-        
-        with open("responses.json", "w") as outfile:
-            json.dump(responses, outfile)
+            streamlit.sidebar.header("Conversation History")
+            for i, question in enumerate(questions):
+                if i < streamlit.session_state['current_question']:
+                    streamlit.sidebar.write(question['question'])
+                    streamlit.sidebar.write("You: " + streamlit.session_state['past'][i])
 
-        message("Thank you for answering all the questions. Your responses have been saved.", is_user=False)
-        streamlit.sidebar.write("Thank you for answering all the questions. Your responses have been saved.")
+        else:
+            responses = []
+            for i, question in enumerate(questions):
+                response = {
+                    "question": question['question'],
+                    "response": streamlit.session_state['past'][i]
+                }
+                responses.append(response)
+            
+            with open("responses.json", "w") as outfile:
+                json.dump(responses, outfile)
 
-        streamlit.sidebar.download_button(
-            label="Download Responses",
-            data=json.dumps(responses),
-            file_name="responses.json",
-            mime="application/json"
-        )
-        
+            message("Thank you for answering all the questions. Your responses have been saved.", is_user=False)
+            streamlit.sidebar.write("Thank you for answering all the questions. Your responses have been saved.")
+
+            streamlit.sidebar.download_button(
+                label="Download Responses",
+                data=json.dumps(responses),
+                file_name="responses.json",
+                mime="application/json"
+            )
+            
     if streamlit.session_state['generated']:
         for i in range(len(streamlit.session_state['generated'])-1, -1, -1):
             message(streamlit.session_state["generated"][i], key=str(i))
