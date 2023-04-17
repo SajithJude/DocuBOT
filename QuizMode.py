7dab19d911663b2a8b0035da35826d754cf9e34e
Jude Sajith <judesajith.aj@gmail.com>
2023-04-17 10:39:19 +0530

    sss

--- QuizMode.py
+++ QuizMode.py
@@ -1,142 +1,110 @@
-# Import necessary libraries
 import streamlit as st
-from langchain.chains import ConversationChain
-from langchain.chains.conversation.memory import ConversationEntityMemory
-from langchain.chains.conversation.prompt import ENTITY_MEMORY_CONVERSATION_TEMPLATE
-from langchain.llms import OpenAI
-from llama_index import GPTSimpleVectorIndex, Document, SimpleDirectoryReader, QuestionAnswerPrompt
+import pyrebase
+import firebase_admin
+from firebase_admin import credentials, firestore, auth
+import json
+cred = credentials.Certificate("docubot-2ac1d-firebase-adminsdk-9ztu6-80050a35cd.json")
 
-import os 
+def assign_assessment(user_id: str, assessment: dict):
+    db = firestore.client()
+    user_ref: DocumentReference = db.collection("users").document(user_id)
+    user_ref.set({"assessment": assessment}, merge=True)
+    return True
 
-# Set Streamlit page configuration
-st.set_page_config(page_title='DocuBOT Quiz Mode', layout='wide')
-# Initialize session states
-if "generated" not in st.session_state:
-    st.session_state["generated"] = []
-if "past" not in st.session_state:
-    st.session_state["past"] = []
-if "input" not in st.session_state:
-    st.session_state["input"] = ""
-if "stored_session" not in st.session_state:
-    st.session_state["stored_session"] = []
-if "index" not in st.session_state:
-    st.session_state["index"] = None
 
-# Define function to get user input
-def get_text():
-    """
-    Get the user input text.
+# Initialize Pyrebase with the Firebase project credentials
+config = {
+    "apiKey": "AIzaSyCnP2MswW3g6zpdNP0hx3aviXCej2ZmC0c",
+    "authDomain": "docubot-2ac1d.firebaseapp.com",
+    "projectId": "docubot-2ac1d",
+    'databaseURL': "https://docubot-2ac1d-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    "storageBucket": "docubot-2ac1d.appspot.com",
+    "messagingSenderId": "1053457031443",
+    "appId": "1:1053457031443:web:82e2dbbf519bd97435bae6",
+    "measurementId": "G-DM2R9ECXRV"
+}
+firebase = pyrebase.initialize_app(config)
 
-    Returns:
-        (str): The text entered by the user
-    """
-    input_text = st.text_input("You: ", st.session_state["input"], key="input",
-                            placeholder="Ask Me to Quiz you from a chapter and correct your ...", 
-                            label_visibility='hidden')
-    return input_text
+firebase = pyrebase.initialize_app(config)
 
-# Define function to start a new chat
-def new_chat():
-    """
-    Clears session state and starts a new chat.
-    """
-    save = []
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        save.append("User:" + st.session_state["past"][i])
-        save.append("Bot:" + st.session_state["generated"][i])        
-    st.session_state["stored_session"].append(save)
-    st.session_state["generated"] = []
-    st.session_state["past"] = []
-    st.session_state["input"] = ""
-    st.session_state.entity_memory.store = {}
-    st.session_state.entity_memory.buffer.clear()
+def register():
+    st.subheader("Create a new account")
+    name = st.text_input("Name")
+    institute = st.text_input("Institute")
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    role = st.selectbox("User Role", ["learner", "instructor"])
+    if st.button("Register"):
+        try:
+            auth = firebase.auth()
+            user = auth.create_user_with_email_and_password(email, password)
+            db = firestore.client()
 
-# Set up sidebar with various options
-with st.sidebar.expander("Settings ", expanded=False):
-    # # Option to preview memory store
-    # if st.checkbox("Preview memory store"):
-    #     with st.expander("Memory-Store", expanded=False):
-    #         st.session_state.entity_memory.store
-    # # Option to preview memory buffer
-    # if st.checkbox("Preview memory buffer"):
-    #     with st.expander("Bufffer-Store", expanded=False):
-    #         st.session_state.entity_memory.buffer
-    MODEL = st.selectbox(label='Model', options=['gpt-3.5-turbo','text-davinci-003','text-davinci-002','code-davinci-002'])
-    K = st.number_input(' Number of Questions per quiz session',min_value=3,max_value=1000)
-    # Option to load an index
-    # if st.checkbox("Load Index"):
+            db.collection("users").document(user["localId"]).set({
+                "name": name,
+                "institute": institute,
+                "email": email,
+                "role": role
+            })
+            st.success("Account created!")
+        except Exception as e:
+            st.error(e)
 
-    #     st.session_state.index = GPTSimpleVectorIndex.load_from_disk('index.json')
-    #     st.success("Index loaded successfully")
-    #     # index_path = st.text_input("Select an index file")
-    #     # if index_path is not None:
-
-# Set up the Streamlit app layout
-st.title("DocuBot QuizMode")
-# st.subheader(" Powered by 🦜 LangChain + OpenAI + Streamlit")
-
-# Ask the user to enter their OpenAI API key
-API_O = os.getenv("API_KEY")
-
-# Session state storage would be ideal
-iconImage = 'CourseBOT Mascot.png'
-
-# Create an OpenAI instance
-llm = OpenAI(temperature=0,
-            openai_api_key=API_O, 
-            model_name=MODEL, 
-            verbose=False) 
-
-# Create a ConversationEntityMemory object if not already created
-if 'entity_memory' not in st.session_state:
-    st.session_state.entity_memory = ConversationEntityMemory(llm=llm, k=K )
-
-# Create the ConversationChain object with the specified configuration
-Conversation = ConversationChain(
-    llm=llm, 
-    prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
-    memory=st.session_state.entity_memory
-)  
-
-# Generate the output using the ConversationChain object and the user input, and add the input/output to the session
-if st.session_state.index is not None:
-    user_input = get_text()
-    if user_input:
-        response = st.session_state.index.query(user_input)
-        if response is not None:
-            output = response
-            st.session_state.past.append(user_input)  
-            st.session_state.generated.append(output)
-else:
-    user_input = get_text()
-    if user_input:
-        output = Conversation.run(input=user_input)  
-        st.session_state.past.append(user_input)  
-        st.session_state.generated.append(output)  
-
-# Allow to download as well
-download_str = []
-# Display the conversation history using an expander, and allow the user to download it
-with st.expander("Conversation", expanded=True):
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        st.info(st.session_state["past"][i],icon="🧐")
-        st.success(st.session_state["generated"][i], icon="🤖")
-        download_str.append(st.session_state["past"][i])
-        download_str.append(st.session_state["generated"][i])
+st.title("Docubot Flipick")
+menu = ["Home", "Login", "Register"]
+choice = st.selectbox("Select an option", menu)
+if choice == "Home":
+    st.subheader("Welcome to the User Management App")
     
-    # Can throw error - requires fix
-    download_str = '\n'.join(download_str)
-    if download_str:
-        st.download_button('Download',download_str)
+elif choice == "Login":
+    st.subheader("Login to your account")
+    email = st.text_input("Email", value=st.session_state.get("email", ""))
+    password = st.text_input("Password", type="password", value=st.session_state.get("password", ""))
+    if st.button("Login"):
+        try:
+            Auth = firebase.auth()
+            db = firestore.client()
+            Auth.sign_in_with_email_and_password(email, password)
+            st.success("Logged in!")
+            user = auth.get_user_by_email(email)
+            if user is not None:
+                role = db.collection("users").document(user.uid).get().to_dict().get("role")
+                if role == "instructor":
+                    learners = db.collection("users").where("role", "==", "learner").get()
+                    st.subheader("List of Learners:")
+                    for learner in learners:
+                        st.write(f"- {learner.to_dict()['name']}")
 
-# Display stored conversation sessions in the sidebar
-for i, sublist in enumerate(st.session_state.stored_session):
-        with st.sidebar.expander(label= f"Conversation-Session:{i}"):
-            st.write(sublist)
+                    # Add Assign Assessment button and input field
+                    assessment_json = st.session_state.json_output
+                    st.session_state.selected_learner = st.selectbox("Select Learner to Assign Assessment", [learner.to_dict()["name"] for learner in learners], key="learner_selection")
 
-# Allow the user to clear all stored conversation sessions
-if st.session_state.stored_session:   
-    if st.sidebar.checkbox("Clear-all"):
-        del st.session_state.stored_session
+                    # st.session_state.selected_learner = selected_learner
+                    # assign_button = st.button("Assign Assessment")
 
-           
+                    # if st.session_state.selected_learner:
+                    #     print("working assign")
+
+                        # Find the selected learner's ID
+                    st.session_state.selected_learner_id = None
+                    for learner in learners:
+                        if learner.to_dict()["name"] == st.session_state.selected_learner:
+                            st.session_state.selected_learner_id = learner.id
+                            st.write(st.session_state.selected_learner_id)
+                            pass 
+
+                    # Assign the assessment to the selected learner
+                    try:
+                        assessment_data = assessment_json
+                        if assign_assessment(st.session_state.selected_learner_id, assessment_data):
+                            st.success(f"Assessment assigned to {st.session_state.selected_learner}")
+                        else:
+                            st.error("Failed to assign assessment")
+                    except Exception as e:
+                        st.error(f"Invalid JSON format: {e}")
+        except Exception as e:
+            st.error(e)                        
+elif choice == "Register":
+
+# Show the registration form
+    register()
