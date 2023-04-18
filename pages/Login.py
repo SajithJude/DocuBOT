dffb65edd2c9b86f16ceecb831dd2fcffe81d0e6
Thananjayan <thananjayan1991@yahoo.com>
2023-04-18 17:17:20 +0530

    ssss

--- pages/Login.py
+++ pages/Login.py
@@ -1,15 +1,18 @@
 import streamlit as st
 
-tab1, tab2 = st.tabs(["login", "register"])
+container = st.container()
 
-with tab1:
-    st.subheader("Login")
-    user_type = st.radio("Select user type", ("student", "instructor"))
-    email = st.text_input("Email")
-    password = st.text_input("Password", type="password")
-    if st.button("Login"):
-        # Login button logic goes here
-        pass
+with container:
+    tab1, tab2 = st.tabs(["login", "register"])
 
-with tab2:
-    st.subheader("Register")
+    with tab1:
+        st.subheader("Login")
+        user_type = st.radio("Select user type", ("student", "instructor"))
+        email = st.text_input("Email")
+        password = st.text_input("Password", type="password")
+        if st.button("Login"):
+            # Login button logic goes here
+            pass
+
+    with tab2:
+        st.subheader("Register")
