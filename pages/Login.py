0761471a8a080ffccd3d29d05d2dc84d4f8e5e8f
Thananjayan <thananjayan1991@yahoo.com>
2023-04-18 17:03:07 +0530

    5th

--- pages/Login.py
+++ pages/Login.py
@@ -5,14 +5,11 @@ container = st.container()
 container.markdown(
     f'<div style="border-radius: 10px; box-shadow: 0px 0px 5px 2px rgba(0, 0, 0, 0.1); padding: 20px;">'
     f'<h2 style="text-align: center;">Login Form</h2>'
+    f'<form>'
+    f'<label for="email">Email:</label><br>'
+    f'<input type="email" id="email" name="email"><br>'
+    f'<label for="password">Password:</label><br>'
+    f'<input type="password" id="password" name="password"><br><br>'
+    f'<input type="submit" value="Submit">'
+    f'</form>'
     f'</div>', unsafe_allow_html=True)
-
-# Add input fields to the container
-with st.container():
-    email = container.text_input("Email")
-    password = container.text_input("Password", type="password")
-
-# Add submit button to the container
-if container.button("Submit"):
-    st.write(f"Email: {email}")
-    st.write(f"Password: {password}")
