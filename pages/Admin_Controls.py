be17ddcfdcae37484a7987b2d23f64ca905e5dcd
Thananjayan <thananjayan1991@yahoo.com>
2023-04-19 09:13:11 +0530

    ad

--- pages/Admin_Controls.py
+++ pages/Admin_Controls.py
@@ -5,6 +5,9 @@ from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
 import openai
 from pathlib import Path
 from llama_index import download_loader
+from typing import List
+import json
+
 
 openai.api_key = os.getenv("API_KEY")
 PDFReader = download_loader("PDFReader")
@@ -12,12 +15,23 @@ loader = PDFReader()
 
 # Define the data directory path
 DATA_DIR = "data"
-
+DB_FILE = "db.json"
 # Create the data directory if it doesn't exist
 if not os.path.exists(DATA_DIR):
     os.mkdir(DATA_DIR)
 
+
+def load_users_dicts() -> List[dict]:
+    if Path(DB_FILE).is_file():
+        with open(DB_FILE, "r") as f:
+            users_data = json.load(f)
+        return users_data
+    else:
+        return []
+
 # Define a function to display the contents of a PDF file
+
+
 def display_pdf(DATA_DIR, pdf_file):
     with open(os.path.join(DATA_DIR, pdf_file), "rb") as f:
         pdf_reader = PyPDF2.PdfFileReader(f)
@@ -27,9 +41,12 @@ def display_pdf(DATA_DIR, pdf_file):
                 st.write(page.extractText())
 
 # Define a function to delete a PDF file and its corresponding JSON index file
+
+
 def delete_file(DATA_DIR, file_name):
     pdf_path = os.path.join(DATA_DIR, file_name)
-    json_path = os.path.join(DATA_DIR, os.path.splitext(file_name)[0] + ".json")
+    json_path = os.path.join(
+        DATA_DIR, os.path.splitext(file_name)[0] + ".json")
     if os.path.exists(pdf_path):
         os.remove(pdf_path)
         st.success(f"File {file_name} deleted successfully!")
@@ -39,70 +56,80 @@ def delete_file(DATA_DIR, file_name):
         os.remove(json_path)
 
 # Define a function to save the uploaded file to the data directory
+
+
 def save_uploaded_file(uploaded_file):
     with open(os.path.join(DATA_DIR, uploaded_file.name), "wb") as f:
         f.write(uploaded_file.getbuffer())
 
 # Define the Streamlit app
+
+
 def main():
     st.title("DocuBOT Admin")
 
-
     # Create a file uploader widget
     uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
-    
+
     # Check if a file was uploaded
     if uploaded_file is not None:
         # Save the uploaded file to the data directory
         save_uploaded_file(uploaded_file)
         st.success("It would take a while to index the books, please wait..!")
-    
+
     # Create a button to create the index
     # if st.button("Create Index"):
         # Get the filename of the uploaded PDF
         pdf_filename = uploaded_file.name
-        
+
         # Load the documents from the data directory
         documents = SimpleDirectoryReader(DATA_DIR).load_data()
-        
+
         # Create the index from the documents
         index = GPTSimpleVectorIndex.from_documents(documents)
-        
+
         # Save the index to the data directory with the same name as the PDF
-        index.save_to_disk(os.path.join(DATA_DIR, os.path.splitext(pdf_filename)[0] + ".json"))
+        index.save_to_disk(os.path.join(
+            DATA_DIR, os.path.splitext(pdf_filename)[0] + ".json"))
         st.success("Index created successfully!")
-    
+
     # Get a list of files in the directory
     files = os.listdir(DATA_DIR)
-    
+
     # Filter out the JSON index files
     files = [f for f in files if not f.endswith(".json")]
 
     colms = st.columns((4, 1, 1))
 
-    fields = ["Name", 'View', 'Delete' ]
+    fields = ["Name", 'View', 'Delete']
     for col, field_name in zip(colms, fields):
         # header
         col.subheader(field_name)
 
-    i=1
+    i = 1
     for Name in files:
-        i+=1
-        col1, col2, col3 = st.columns((4 , 1, 1))
+        i += 1
+        col1, col2, col3 = st.columns((4, 1, 1))
         # col1.write(x)  # index
         col1.caption(Name)  # email
         if Name.endswith(".pdf"):
-            col2.button("View", key=Name, on_click=display_pdf, args=(DATA_DIR, Name))  # unique ID
+            col2.button("View", key=Name, on_click=display_pdf,
+                        args=(DATA_DIR, Name))  # unique ID
             delete_status = True
         else:
             col2.write("N/A")
             delete_status = False
         button_type = "Delete" if delete_status else "Gone"
         button_phold = col3.empty()  # create a placeholder
-        do_action = button_phold.button(button_type, key=i, on_click=delete_file, args=(DATA_DIR, Name))
-    
+        do_action = button_phold.button(
+            button_type, key=i, on_click=delete_file, args=(DATA_DIR, Name))
+
+    users_dicts = load_users_dicts()
+    st.table(users_dicts)
+
+
 if __name__ == "__main__":
-    password = st.text_input("Enter password",type="password")
+    password = st.text_input("Enter password", type="password")
     if password == "flipick":
         main()
     else:
