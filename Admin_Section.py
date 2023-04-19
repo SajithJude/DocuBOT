3b242c973107907722d3996c0a54e486f3048473
Jude Sajith <judesajith.aj@gmail.com>
2023-04-19 11:34:39 +0530

    ssss

--- Admin_Section.py
+++ Admin_Section.py
@@ -23,6 +23,8 @@ show_pages(
 DB_FILE = "db.json"
 
 
+
+
 class User:
     def __init__(self, username, password, user_type, instructor=None, assignments=None):
         self.username = username
@@ -56,58 +58,71 @@ def save_users(users: List[User]):
         json.dump(users_data, f)
 
 
-
 def main():
-    st.title("Login or Register")
+    set_style()
 
     users = load_users()
 
-    if "username" not in st.session_state:
-        username = st.text_input("Username", value="learner1")
-        password = st.text_input("Password",value="password3" ,type="password")
+    container = st.container()
 
-        if st.button("Login"):
-            user = [user for user in users if user.username == username and user.password == password]
-            if user:
-                user = user[0]
-                st.session_state['username'] = user.username
-                st.session_state['user_type'] = user.user_type
-                st.write(f"Logged in as {user.username} ({user.user_type}).")
+    with container:
+        tab1, tab2 = st.tabs(["Login", "Register"])
+
+        with tab1:
+            st.subheader("Login")
+            username = st.text_input("Username")
+            password = st.text_input("Password", type="password")
+
+            if st.button("Login"):
+                user = [user for user in users if user.username ==
+                        username and user.password == password]
+                if user:
+                    user = user[0]
+                    st.session_state['username'] = user.username
+                    st.session_state['user_type'] = user.user_type
+                    st.success(
+                        f"Logged in as {user.username} ({user.user_type}).")
+                  
+                else:
+                    st.write("Invalid username or password.")
+
+        with tab2:
+            st.subheader("Register")
+            user_type = st.radio("Select user type",
+                                 ("student", "instructor"), horizontal=True,)
+            if user_type == "instructor":
+                username_reg = st.text_input("Username (Instructor)")
+                password_reg = st.text_input(
+                    "Password (Instructor)", type="password")
+                if username_reg and password_reg:
+                    new_user = User(username_reg, password_reg, user_type)
+                    users.append(new_user)
+                    save_users(users)
+                    st.success(
+                        f"User {username_reg} registered successfully as a {user_type}.")
             else:
-                st.write("Invalid username or password.")
-    else:
-        user = [u for u in users if u.username == st.session_state['username']][0]
-        if user.user_type == "instructor":
-            st.write(f"Welcome, {user.username}! You are an instructor.")
-            st.write("List of your students:")
-            students = [u for u in users if u.user_type == "learner" and u.instructor == user.username]
-            for student in students:
-                st.write(student.username)
-        else:
-            st.write(f"Welcome, {user.username}! You are a learner.")
-            st.write(f"Your instructor is: {user.instructor}")
-            st.write("Your assignments:")
-            for assignment in user.assignments:
-                st.write(assignment)
-                
+                instructors = [
+                    user for user in users if user.user_type == "instructor"]
+                instructor_usernames = [
+                    instructor.username for instructor in instructors]
+                selected_instructor = st.selectbox(
+                    "Select an Instructor", instructor_usernames)
+                username_reg = st.text_input("Username (Learner)")
+                password_reg = st.text_input(
+                    "Password (Learner)", type="password")
 
-    st.title("Register")
-    user_type = st.selectbox("User Type", ["learner", "instructor"])
-    if user_type == "learner":
-        instructors = [user for user in users if user.user_type == "instructor"]
-        instructor_usernames = [instructor.username for instructor in instructors]
-        selected_instructor = st.selectbox("Select an Instructor", instructor_usernames)
+            if st.button("Register"):
+                if not username_reg or not password_reg:
+                    st.write("Please enter a username and password.")
+                else:
+                    new_user = User(
+                        username_reg, password_reg, user_type, instructor=selected_instructor)
 
-    if st.button("Register"):
-        if user_type == "instructor":
-            new_user = User(username, password, user_type)
-        else:
-            new_user = User(username, password, user_type, selected_instructor)
+                    users.append(new_user)
+                    save_users(users)
+                    st.success(
+                        f"User {username_reg} registered successfully as a {user_type}.")
 
-        users.append(new_user)
-        save_users(users)
-        st.write(f"User {username} registered successfully as a {user_type}.")
- 
     if st.button("Logout"):
         keys_to_remove = list(st.session_state.keys())  # Get a list of all session state keys
         for key in keys_to_remove:
