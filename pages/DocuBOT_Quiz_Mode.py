fc10c64627c40028dfc6cad81aad40dc211668cd
Jude Sajith <judesajith.aj@gmail.com>
2023-04-19 08:36:40 +0530

    sss

--- pages/DocuBOT_Quiz_Mode.py
+++ pages/DocuBOT_Quiz_Mode.py
@@ -1,10 +1,14 @@
 import streamlit as st
+from streamlit_chat import message
 import json
 from typing import List
 from pathlib import Path
 
 DB_FILE = "db.json"
 
+def get_text():
+    input_text = st.text_input("You: ","", key="input")
+    return input_text
 
 class User:
     def __init__(self, username, password, user_type, instructor=None, assignments=None):
@@ -52,27 +56,39 @@ if "username" in st.session_state:
     user = [u for u in users if u.username == st.session_state['username']][0]
     if user.user_type == "learner":
         questions = user.assignments
+        # st.write(questions)
+        if 'generated' not in st.session_state:
+            st.session_state['generated'] = []
 
         if 'past' not in st.session_state:
             st.session_state['past'] = []
-
+            
         if 'current_question' not in st.session_state:
             st.session_state['current_question'] = 0
 
-        def get_text():
-            input_text = st.text_input("You: ", "", key="input")
-            submit_button = st.button("Submit Answer", key="submit_button")
-            return input_text, submit_button
+         
+        # submit = st.button("next",key="submit")
 
-        if st.session_state['current_question'] < len(questions):
+       if st.session_state['current_question'] < len(questions):
             current_question = questions[st.session_state['current_question']]
-            st.write(f"Bot: {current_question['question']}")
-            user_input, submit_button = get_text()
+            message(current_question['question'], is_user=False, key=str(st.session_state['current_question']))
+
+            with st.form(key="input_form"):
+                user_input = st.text_input("You: ", "", key="input")
+                submit_button = st.form_submit_button(label="Submit")
 
             if submit_button and user_input:
                 st.session_state['past'].append(user_input)
                 st.session_state['current_question'] += 1
-                st.text_input("You: ", value="", key="input")  # Reset the text input field
+                    
+                    
+
+            st.sidebar.header("Conversation History")
+            for i, question in enumerate(questions):
+                if i < st.session_state['current_question']:
+                    st.sidebar.write(question['question'])
+                    st.sidebar.write("You: " + st.session_state['past'][i])
+
 
         else:
             responses = []
@@ -82,17 +98,27 @@ if "username" in st.session_state:
                     "response": st.session_state['past'][i]
                 }
                 responses.append(response)
-
+            
+            # with open("responses.json", "w") as outfile:
+            #     json.dump(responses, outfile)
             user.assignments = responses
             save_users(users)
 
-            st.write("Thank you for answering all the questions. Your responses have been saved.")
-            st.download_button(
+            message("Thank you for answering all the questions. Your responses have been saved.", is_user=False)
+            st.sidebar.write("Thank you for answering all the questions. Your responses have been saved.")
+
+            st.sidebar.download_button(
                 label="Download Responses",
                 data=json.dumps(responses),
                 file_name="responses.json",
                 mime="application/json"
             )
+
+    if st.session_state['generated']:
+        for i in range(len(st.session_state['generated'])-1, -1, -1):
+            message(st.session_state["generated"][i], key=str(i))
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+            st.sidebar.write("Bot: ", st.session_state["generated"][i])
+
 else:
     st.info("Please Login or Register")
-
