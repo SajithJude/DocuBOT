1230254f346134ce6761a9397226068fc134b97d
Thananjayan <thananjayan1991@yahoo.com>
2023-04-20 09:15:56 +0530

    ssc

--- Admin_Section.py
+++ Admin_Section.py
@@ -4,6 +4,9 @@ from typing import List
 from pathlib import Path
 from st_pages import Page, Section, show_pages, add_page_title
 
+favicon = "favicon.ac8d93a.69085235180674d80d902fdc4b848d0b (1).png"
+st.set_page_config(page_title="DocuBOT", page_icon=favicon)
+st.image("Flipick_Logo-1 (1)-fotor-bg-remover-20230419132039.png", width=150)
 
 show_pages(
     [
@@ -95,30 +98,30 @@ def main():
                     st.write(
                         f"Logged in as {user.username} ({user.user_type}).")
 
-                    if user_type == 'instructor':
-                        show_pages(
-                            [
-                                Page("Admin_Section.py", "Home", "🏠"),
-                                Page("pages/Instructor_Quiz_Config.py",
-                                     "Instructor_Quiz_Config", "💪"),
-                                Page("pages/DocuBOT.py", "DocuBOT", "🎈️")
-                                # Section("My section", icon="🎈️"),
-                                # Pages after a section will be indented
-                                # Page("DocuBOT_Quiz.py",
-                                #  "DocuBot Quiz", icon="💪"),
-                                # Unless you explicitly say in_section=False
-                                # Page("Not in a section", in_section=False)
-                            ]
-                        )
-                    else:
-                        show_pages(
-                            [
-                                Page("Admin_Section.py", "Home", "🏠"),
-                                Page("pages/DocuBot_Quiz_Mode.py",
-                                     "DocuBot_Quiz_Mode", "💪"),
-                                Page("pages/DocuBot.py", "Page 2", "🎈️"),
-                            ]
-                        )
+                    # if user_type == 'instructor':
+                    #     show_pages(
+                    #         [
+                    #             Page("Admin_Section.py", "Home", "🏠"),
+                    #             Page("pages/Instructor_Quiz_Config.py",
+                    #                  "Instructor_Quiz_Config", "💪"),
+                    #             Page("pages/DocuBOT.py", "DocuBOT", "🎈️")
+                    #             # Section("My section", icon="🎈️"),
+                    #             # Pages after a section will be indented
+                    #             # Page("DocuBOT_Quiz.py",
+                    #             #  "DocuBot Quiz", icon="💪"),
+                    #             # Unless you explicitly say in_section=False
+                    #             # Page("Not in a section", in_section=False)
+                    #         ]
+                    #     )
+                    # else:
+                    #     show_pages(
+                    #         [
+                    #             Page("Admin_Section.py", "Home", "🏠"),
+                    #             Page("pages/DocuBot_Quiz_Mode.py",
+                    #                  "DocuBot_Quiz_Mode", "💪"),
+                    #             Page("pages/DocuBot.py", "Page 2", "🎈️"),
+                    #         ]
+                    #     )
                 else:
                     st.write("Invalid username or password.")
 
@@ -156,126 +159,38 @@ def main():
 
                     users.append(new_user)
                     save_users(users)
-                    st.write(
+                    st.success(
                         f"User {username_reg} registered successfully as a {user_type}.")
-
-    if st.button("Logout"):
-        st.session_state.pop('username', None)
-        st.session_state.pop('user_type', None)
+    if st.sidebar.button("Logout"):
+        # Get a list of all session state keys
+        keys_to_remove = list(st.session_state.keys())
+        for key in keys_to_remove:
+            # Remove each key from the session state
+            st.session_state.pop(key, None)
         st.write("Logged out successfully.")
-        # show_pages(
-        #     [
-        #         Page("Admin_Section.py", "Home", "🏠"),
-        #         Page("pages/Login_new.py", "Login / Signup", "🎈️"),
-        #         Page("pages/Admin_Controls.py", "Admin Control", "💪"),
-        #     ]
-        # )
+
+    # if st.button("Logout"):
+    #     st.session_state.pop('username', None)
+    #     st.session_state.pop('user_type', None)
+    #     st.write("Logged out successfully.")
+    #     # show_pages(
+    #     #     [
+    #     #         Page("Admin_Section.py", "Home", "🏠"),
+    #     #         Page("pages/Login_new.py", "Login / Signup", "🎈️"),
+    #     #         Page("pages/Admin_Controls.py", "Admin Control", "💪"),
+    #     #     ]
+    #     # )
 
 
 if __name__ == "__main__":
     main()
-
-# class User:
-#     def __init__(self, username, password, user_type, instructor=None, assignments=None):
-#         self.username = username
-#         self.password = password
-#         self.user_type = user_type
-#         self.instructor = instructor
-#         self.assignments = assignments if assignments else []
-
-#     def to_dict(self):
-#         return {
-#             "username": self.username,
-#             "password": self.password,
-#             "user_type": self.user_type,
-#             "instructor": self.instructor,
-#             "assignments": self.assignments,
-#         }
-
-
-# def load_users() -> List[User]:
-#     if Path(DB_FILE).is_file():
-#         with open(DB_FILE, "r") as f:
-#             users_data = json.load(f)
-#         return [User(**user_data) for user_data in users_data]
-#     else:
-#         return []
-
-
-# def save_users(users: List[User]):
-#     users_data = [user.to_dict() for user in users]
-#     with open(DB_FILE, "w") as f:
-#         json.dump(users_data, f)
-
-
-# def main():
-#     st.title("Login or Register")
-
-#     users = load_users()
-
-#     if "username" not in st.session_state:
-#         username = st.text_input("Username")
-#         password = st.text_input("Password", type="password")
-
-#         if st.button("Login"):
-#             user = [user for user in users if user.username ==
-#                     username and user.password == password]
-#             if user:
-#                 user = user[0]
-#                 st.session_state['username'] = user.username
-#                 st.session_state['user_type'] = user.user_type
-#                 st.write(f"Logged in as {user.username} ({user.user_type}).")
-#             else:
-#                 st.write("Invalid username or password.")
-#     else:
-#         user = [u for u in users if u.username ==
-#                 st.session_state['username']][0]
-#         if user.user_type == "instructor":
-#             st.write(f"Welcome, {user.username}! You are an instructor.")
-#             st.write("List of your students:")
-#             students = [u for u in users if u.user_type ==
-#                         "learner" and u.instructor == user.username]
-#             for student in students:
-#                 st.write(student.username)
-#         else:
-#             st.write(f"Welcome, {user.username}! You are a learner.")
-#             st.write(f"Your instructor is: {user.instructor}")
-#             # st.write("Your assignments:")
-#             # for assignment in user.assignments:
-#             #     st.write(assignment)
-
-#     st.title("Register")
-#     user_type = st.selectbox("User Type", ["learner", "instructor"])
-#     if user_type == "learner":
-#         instructors = [
-#             user for user in users if user.user_type == "instructor"]
-#         instructor_usernames = [
-#             instructor.username for instructor in instructors]
-#         selected_instructor = st.selectbox(
-#             "Select an Instructor", instructor_usernames)
-
-#     if st.button("Register"):
-#         if user_type == "instructor":
-#             new_user = User(username, password, user_type)
-#         else:
-#             new_user = User(username, password, user_type, selected_instructor)
-
-#         users.append(new_user)
-#         save_users(users)
-#         st.write(f"User {username} registered successfully as a {user_type}.")
-
-#     if st.button("Logout"):
-#         st.session_state.pop('username', None)
-#         st.session_state.pop('user_type', None)
-#         st.write("Logged out successfully.")
-#         show_pages(
-#             [
-#                 Page("Admin_Section.py", "Home", "🏠"),
-#                 Page("pages/Login_new.py", "Login / Signup", "🎈️"),
-#                 Page("pages/Admin_Controls.py", "Admin Control", "💪"),
-#             ]
-#         )
-
-
-# if __name__ == "__main__":
-#     main()
+    if "username" in st.session_state:
+        users = load_users()
+        user = [u for u in users if u.username ==
+                st.session_state['username']][0]
+        if user.user_type == "instructor":
+            show_pages([
+                Page("Admin_Section.py", "Home", "🏠"),
+                Page("pages/Admin_Controls.py",  "Admin_Controls"),
+                Page("pages/Instructor_Quiz_Config.py",  "Question_Generation")
+            ])
