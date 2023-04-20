5b1abcf9da2d4dc60b7f3ba956a93fad8466b5c5
Thananjayan <thananjayan1991@yahoo.com>
2023-04-20 09:51:51 +0530

    aaa

--- pages/Instructor_Quiz_Config.py
+++ pages/Instructor_Quiz_Config.py
@@ -4,7 +4,6 @@ import os
 import json
 import base64
 from typing import List
-# import streamlit as st
 from pathlib import Path
 import json
 
@@ -58,10 +57,7 @@ def main():
         # uploaded_file = st.file_uploader("Upload the responses JSON file here")
 
         # if uploaded_file is not None:
-        try:
-            responses = st.session_state.json_output
-        except AttributeError:
-            st.info("generate questions to get started")
+        responses = st.session_state.json_output
 
         # Choose a student to assign the responses
         students = [u for u in users if u.user_type ==
@@ -85,140 +81,133 @@ def main():
 
 
 if __name__ == "__main__":
-    users = load_users()
+
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    st.subheader("Question & Answer Generation Admin section")
+
+    form = """
+    [
+    {
+        "question": "Question here?",
+        "answer": "Answer here.",
+        "Question": "Question here?"
+    },
+    { 
+        "question": "Question here?",
+        "answer": "Answer here.",
+        "Question": "Question here?"
+    }
+    ]
+    """
+
+    topic = st.text_input("Enter topic here")
+    num_quest = st.slider('Number of questions to generate', 0, 10, 1)
+    result = st.button("Submit")
+
+    if result:
+        prompt = f"generate {num_quest} essay type questions with answers on the topic of {topic}, with the all the possible correct comprehensive answers, show the output in following json list format:\n {form}."
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            temperature=0.56,
+            max_tokens=2100,
+            top_p=1,
+            frequency_penalty=0.35,
+            presence_penalty=0
+        )
+        output = response.choices[0].text.strip()
+        json_output = json.loads(output)
+        # st.write(json_output)
+        # Initialization
+
+    # Session State also supports attribute based syntax
+        if 'json_output' not in st.session_state:
+            st.session_state.json_output = json_output
+    # except AttributeError:
+
+    try:
+        st.subheader("Refining section by subject matter expert")
+        # Display the JSON output as editable text_input fields
+        for i, item in enumerate(st.session_state.json_output):
+            st.write(f"question {i+1}")
+            question = st.text_input("question", item["question"])
+            answer = st.text_area("answer", item["answer"])
+
+            # Update the JSON output with the edited fields
+            st.session_state.json_output[i]["Question"] = question
+            st.session_state.json_output[i]["answer"] = answer
+
+        # Display a download button to download the edited version
+        edited_json = json.dumps(st.session_state.json_output, indent=2)
+    except AttributeError:
+        st.warning("Type a topic and generate some questions to refine them")
+
     main()
 
-    if "username" in st.session_state:
-        user = [u for u in users if u.username ==
-                st.session_state['username']][0]
-        if user.user_type == "instructor":
 
-            openai.api_key = os.getenv("OPENAI_API_KEY")
+# openai.api_key = os.getenv("OPENAI_API_KEY")
 
-            st.subheader("Question & Answer Generation Admin section")
 
-            form = """
-            [
-            {
-                "question": "Question here?",
-                "answer": "Answer here.",
-                "Question": "Question here?"
-            },
-            { 
-                "question": "Question here?",
-                "answer": "Answer here.",
-                "Question": "Question here?"
-            }
-            ]
-            """
+# st.subheader("Question & Answer Generation Admin section")
 
-            topic = st.text_input("Enter topic here")
-            num_quest = st.slider('Number of questions to generate', 0, 10, 1)
-            result = st.button("Submit")
 
-            if result:
-                prompt = f"generate {num_quest} essay type questions with answers on the topic of {topic}, with the all the possible correct comprehensive answers, show the output in following json list format:\n {form}."
-                response = openai.Completion.create(
-                    model="text-davinci-003",
-                    prompt=prompt,
-                    temperature=0.56,
-                    max_tokens=2100,
-                    top_p=1,
-                    frequency_penalty=0.35,
-                    presence_penalty=0
-                )
-                output = response.choices[0].text.strip()
-                json_output = json.loads(output)
-                # st.write(json_output)
-                # Initialization
+# form = """
+# [
+#   {
+#     "question": "Question here?",
+#     "answer": "Answer here.",
+#     "Question": "Question here?"
+#   },
+#   {
+#     "question": "Question here?",
+#     "answer": "Answer here.",
+#     "Question": "Question here?"
+#   }
+# ]
+# """
 
-            # Session State also supports attribute based syntax
-                if 'json_output' not in st.session_state:
-                    st.session_state.json_output = json_output
-            # except AttributeError:
+# topic = st.text_input("Enter topic here")
+# num_quest = st.slider('Number of questions to generate', 0, 10, 1)
+# result = st.button("Submit")
 
-            try:
-                st.subheader("Refining section by subject matter expert")
-                # Display the JSON output as editable text_input fields
-                for i, item in enumerate(st.session_state.json_output):
-                    st.write(f"question {i+1}")
-                    question = st.text_input("question", item["question"])
-                    answer = st.text_area("answer", item["answer"])
+# if result:
+#     prompt = f"generate {num_quest} essay type questions with answers on the topic of {topic}, with the all the possible correct comprehensive answers, show the output in following json list format:\n {form}."
+#     response = openai.Completion.create(
+#         model="text-davinci-003",
+#         prompt=prompt,
+#         temperature=0.56,
+#         max_tokens=2100,
+#         top_p=1,
+#         frequency_penalty=0.35,
+#         presence_penalty=0
+#     )
+#     output = response.choices[0].text.strip()
+#     json_output = json.loads(output)
+#     # st.write(json_output)
+#     # Initialization
 
-                    # Update the JSON output with the edited fields
-                    st.session_state.json_output[i]["Question"] = question
-                    st.session_state.json_output[i]["answer"] = answer
+# # Session State also supports attribute based syntax
+#     if 'json_output' not in st.session_state:
+#         st.session_state.json_output = json_output
+# # except AttributeError:
 
-                # Display a download button to download the edited version
-                edited_json = json.dumps(
-                    st.session_state.json_output, indent=2)
-            except AttributeError:
-                st.warning(
-                    "Type a topic and generate some questions to refine them")
+# try:
+#     st.subheader("Refining section by subject matter expert")
+#         # Display the JSON output as editable text_input fields
+#     for i, item in enumerate(st.session_state.json_output):
+#         st.write(f"question {i+1}")
+#         question = st.text_input("question", item["question"])
+#         answer = st.text_area("answer", item["answer"])
 
-    else:
-        st.info("Please Login or Register")
+#         # Update the JSON output with the edited fields
+#         st.session_state.json_output[i]["Question"] = question
+#         st.session_state.json_output[i]["answer"] = answer
 
-        # openai.api_key = os.getenv("OPENAI_API_KEY")
-
-        # st.subheader("Question & Answer Generation Admin section")
-
-        # form = """
-        # [
-        #   {
-        #     "question": "Question here?",
-        #     "answer": "Answer here.",
-        #     "Question": "Question here?"
-        #   },
-        #   {
-        #     "question": "Question here?",
-        #     "answer": "Answer here.",
-        #     "Question": "Question here?"
-        #   }
-        # ]
-        # """
-
-        # topic = st.text_input("Enter topic here")
-        # num_quest = st.slider('Number of questions to generate', 0, 10, 1)
-        # result = st.button("Submit")
-
-        # if result:
-        #     prompt = f"generate {num_quest} essay type questions with answers on the topic of {topic}, with the all the possible correct comprehensive answers, show the output in following json list format:\n {form}."
-        #     response = openai.Completion.create(
-        #         model="text-davinci-003",
-        #         prompt=prompt,
-        #         temperature=0.56,
-        #         max_tokens=2100,
-        #         top_p=1,
-        #         frequency_penalty=0.35,
-        #         presence_penalty=0
-        #     )
-        #     output = response.choices[0].text.strip()
-        #     json_output = json.loads(output)
-        #     # st.write(json_output)
-        #     # Initialization
-
-        # # Session State also supports attribute based syntax
-        #     if 'json_output' not in st.session_state:
-        #         st.session_state.json_output = json_output
-        # # except AttributeError:
-
-        # try:
-        #     st.subheader("Refining section by subject matter expert")
-        #         # Display the JSON output as editable text_input fields
-        #     for i, item in enumerate(st.session_state.json_output):
-        #         st.write(f"question {i+1}")
-        #         question = st.text_input("question", item["question"])
-        #         answer = st.text_area("answer", item["answer"])
-
-        #         # Update the JSON output with the edited fields
-        #         st.session_state.json_output[i]["Question"] = question
-        #         st.session_state.json_output[i]["answer"] = answer
-
-        #     # Display a download button to download the edited version
-        #     edited_json = json.dumps(st.session_state.json_output, indent=2)
-        #     b64 = base64.b64encode(edited_json.encode()).decode()
-        #     href = f'<a href="data:file/json;base64,{b64}" download="{topic}.json">Download edited JSON file</a>'
-        #     st.markdown(href, unsafe_allow_html=True)
-        # except AttributeError:
-        #     st.warning("Type a topic and generate some questions to refine them")
+#     # Display a download button to download the edited version
+#     edited_json = json.dumps(st.session_state.json_output, indent=2)
+#     b64 = base64.b64encode(edited_json.encode()).decode()
+#     href = f'<a href="data:file/json;base64,{b64}" download="{topic}.json">Download edited JSON file</a>'
+#     st.markdown(href, unsafe_allow_html=True)
+# except AttributeError:
+#     st.warning("Type a topic and generate some questions to refine them")
