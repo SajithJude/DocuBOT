6bb410158b63886bb2df5ef212ac4662162efd48
Thananjayan <thananjayan1991@yahoo.com>
2023-04-20 21:33:09 +0530

    cdfv

--- pages/My_Profile.py
+++ pages/My_Profile.py
@@ -31,7 +31,7 @@ if st.button("Logout"):
         st.session_state.pop(key, None)
     st.write("Logged out successfully.")
     # switch_page('Admin_Section')
-    show_pages([
-        Page("Admin_Section.py", "Home", "🏠")
-    ])
+    # show_pages([
+    #     Page("Admin_Section.py", "Home", "🏠")
+    # ])
     # st.experimental_rerun()
